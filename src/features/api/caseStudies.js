b48e19abdc9d76d8f794f871dc843687b5abbe6d
Justin Liu <justinliu@Justins-MacBook-Pro.local>
2022-06-24 15:49:39 +0100

    Update carousel block

--- src/features/api/caseStudies.js
+++ src/features/api/caseStudies.js
@@ -6,6 +6,38 @@ const TAGS = {
 
 export const caseStudies = [
     {
+        id: 0,
+        title: "Justin's ski trip",
+        slug: "justins-ski-trip",
+        description: "<p>[Company A] asked us to provide a beautiful stunnning advert for [insert concept]. We went out of bounds on the imagination and overboard on the special effects because that’s what we do.</p>",
+        image: {
+            src: "https://images.unsplash.com/photo-1542038784456-1ea8e935640e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+            alt: "Blah Blah"
+        },
+        tags: [TAGS.VIDEO, TAGS.DIGITAL],
+        metaInfo: {
+            director: {
+                name: "Jamie Kirby",
+                link: ""
+            },
+            music: {
+                name: "Music artist name",
+                link: ""
+            },
+            editor: {
+                name: "Editor name",
+                link: ""
+            },
+            client: {
+                name: "Client name",
+                link: ""
+            },
+        },
+        videoEmbed: "https://player.vimeo.com/video/723742545?h=f7ea89074d",
+        dateCreated: "",
+        datePublished: ""
+    },
+    {
         id: 1,
         title: "Sip & Sit Promo Video",
         slug: "sip-and-sit-promo-video",
